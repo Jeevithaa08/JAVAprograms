e2e4c77d98eada92b9743a53d55f3964019bb96f
Jeevithaa08 <jeevitha08102005@gmail.com>
2025-03-17 20:57:38 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,12 @@
+package Day1program;
+import java.uril.*;
+public class User {
+	/*public static void main(String[] args) {
+		System.out.println("Name : Jeevitha S")
+		System.out.println("Class : 2 A")
+		System.out.println("College : KG")
+		System.out.println("Age : 19")
+		System.out.println("Location : Coimbatore")*/
+	}
+
+}
