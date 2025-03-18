a74f7497d460137b5c2e6fcf26e32064c1e37753
Jeevithaa08 <jeevitha08102005@gmail.com>
2025-03-18 19:56:00 +0530

    Add files via upload

---
+++ ArrayCopy.java
@@ -0,0 +1,28 @@
+package day2project;
+import java.util.Arrays;
+import java.util.Scanner.*;
+public class ArrayCopy {
+	/*public static void main(String[] args) {
+		int[] source = {1,2,3,4,5};
+		int[] destination = new int[5];
+		System.arraycopy(source, 0, destination, 0, source.length);
+		  //copy array
+		for (int num : destination) {
+			System.out.print(num + " ");
+			
+		}*/
+
+	
+	  /*public static void main(String[] args) {
+		int[] numbers = {1,2,3,4,5};
+		      //sort array
+		Arrays.sort(numbers);
+		System.out.println("Sourted Array: " + Arrays.toString(numbers));
+	}*/
+	
+	
+	
+	
+	
+    }	
+}
